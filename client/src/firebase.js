525e0805ecf167c6d0b4a55ea69efa96b5d70ae0
Aminuv <mohamed.elmarbouh07@gmail.com>
2023-12-20 13:25:48 +0000

    fire base for google sign in

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,9 +6,9 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
+  authDomain: 'A-school.firebaseapp.com',
+  projectId: 'A-school',
+  storageBucket: 'A-school.appspot.com',
   messagingSenderId: '1078482850952',
   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
 };
